bb080e8273cf1d63239b588f1b5f9cbcbe063dea
이서진 <seojin-l>
2026-05-14 18:52:33 +0900

    키올렸네 미친

--- src/main/java/com/study/safepetit/analysis/controller/AnalysisController.java
+++ src/main/java/com/study/safepetit/analysis/controller/AnalysisController.java
@@ -3,7 +3,9 @@ package com.study.safepetit.analysis.controller;
 import com.study.safepetit.analysis.dto.AnalysisRequestDto;
 import com.study.safepetit.analysis.dto.AnalysisResponseDto;
 import com.study.safepetit.analysis.service.GeminiService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,7 +17,10 @@ import org.springframework.web.multipart.MultipartFile;
 public class AnalysisController {
     private final GeminiService geminiService;
 
-    @PostMapping
+    @PostMapping(
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    @Operation(summary = "이미지 안전 분석")
     public ResponseEntity<AnalysisResponseDto> analyze(
             @RequestParam Integer childAgeMonth,
             @RequestParam Double childHeight,
@@ -31,5 +36,10 @@ public class AnalysisController {
         String result = geminiService.analyzeSafety(request, image);
 
         return ResponseEntity.ok(new AnalysisResponseDto(result));
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
     }
 }
